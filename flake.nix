00fb4b712bd556b8c2846152a5f07c96c1e49d8d
Patrick <patrick@useless.systems>
2023-09-11 23:34:38 +0000

    rework

--- flake.nix
+++ flake.nix
@@ -1,61 +1,96 @@
 {
-  description = "SolarOS test flake please ignore";
-  
+  description = "Your new nix config";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # hardware.url = "github:nixos/nixos-hardware";
+
     helix.url = "github:helix-editor/helix/23.05";
-    
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dotfiles = {
-      url = "gitlab:aoeuid/dotfiles"; # TODO probably want to clean this up?
-      flake = false;
-    };
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, hyprland, hyprland-contrib, helix, dotfiles, ... }: let
-    system = "x86_64-linux";
-    specialArgs = { inherit home-manager hyprland hyprland-contrib helix dotfiles; };
-    inputModules = [
-      hyprland.nixosModules.default
-      {
-        programs.hyprland.enable = true;
-        programs.hyprland.xwayland.enable = true;
-        programs.hyprland.nvidiaPatches = true;
-      }
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    in
+    rec {
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
 
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = specialArgs;
-        home-manager.users.solar = {
-          imports = [
-            hyprland.homeManagerModules.default # OK, What The Fuck. Why does having this make the way.winMan.hypr line work...?
-            ./home.nix
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      homeManagerModules = import ./modules/home-manager;
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # Desktop
+        solaros = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/solaros ];
+        };
+        # FL Server
+        pluto = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/pluto ];
+        };
+        # CO Server
+        jupiter = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/jupiter ];
+        };
+        # Laptop
+        mercury = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/mercury ];
+        };
+      };
+
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#username@hostname'
+      homeConfigurations = {
+        "patrick@solaros" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home-manager/home.nix
           ];
         };
-      }
-      ./configuration.nix
-    ];
-    
-  in {
-    nixosConfigurations."solaros" = nixpkgs.lib.nixosSystem {
-      inherit system specialArgs;
-      modules = inputModules ++ [];# TODO modularize stuff I guess
+      };
     };
-  };
 }
