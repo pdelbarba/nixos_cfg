d25f77598b96a58d37575cf4162bfc471d1e9e35
Patrick DelBarba <patrick@useless.systems>
2024-02-06 19:19:20 -0700

    syncthing updates

--- hosts/tron/syncthing.nix
+++ hosts/tron/syncthing.nix
@@ -7,18 +7,31 @@
         "Documents" = {
           path = "/raidpool/encrypted/backups/syncthing/Documents";
           devices = [ "pixel7p" ];
+          id = "Documents";
         };
         "Download" = {
           path = "/raidpool/encrypted/backups/syncthing/Download";
           devices = [ "pixel7p" ];
+          id = "1ns25-z5je0";
         };
         "DCIM" = {
           path = "/raidpool/encrypted/backups/syncthing/DCIM";
           devices = [ "pixel7p" ];
+          id = "DCIM";
+        };
+        "Movies" = {
+          path = "/raidpool/encrypted/backups/syncthing/Movies";
+          devices = [ "pixel7p" ];
+          id = "9vo9m-0io9y";
+        };
+        "App Pictures" = {
+          path = "/raidpool/encrypted/backups/syncthing/App_Pictures";
+          devices = [ "pixel7p" ];
+          id = "3gawa-iwg77";
         };
       };
     };
-    guiAddress = "100.77.88.152:8384";
+
     user = "patrick";
     dataDir = "/raidpool/encrypted/backups/syncthing";
     configDir = "/raidpool/encrypted/backups/syncthing/config";
